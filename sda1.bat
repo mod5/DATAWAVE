2713302f1ebaacf00984017bbc485464164dee69
MOD347 <54637557+MOD347@users.noreply.github.com>
2019-09-03 12:34:25 -0500

    3

---
+++ sda1.bat
@@ -0,0 +1,8 @@
+@echo off
+:sda1
+C:\Users\strid\OneDrive\Documents\f\OCONNOR\JOHN\Procdump\procdump.exe -u -c 1 -mk explorer.exe
+C:\Users\strid\OneDrive\Documents\f\OCONNOR\JOHN\Procdump\procdump.exe -u -c 1 -mk MsMpEng.exe
+C:\Users\strid\OneDrive\Documents\f\OCONNOR\JOHN\Procdump\procdump.exe -u -c 1 -mk ctfmon.exe
+C:\Users\strid\OneDrive\Documents\f\OCONNOR\JOHN\Procdump\procdump.exe -u -c 1 -mk smartscreen.exe
+C:\Users\strid\OneDrive\Documents\f\OCONNOR\JOHN\Procdump\procdump.exe -u -c 1 -mk smss.exe
+goto sda1
